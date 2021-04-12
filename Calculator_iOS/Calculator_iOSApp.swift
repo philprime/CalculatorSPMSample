f586dffabf5daddf5ea102b6dbc68a7ea50af4d5
Philip Niedertscheider <dev@philpri.me>
2021-04-12 11:03:36 +0200

    Added iOS app and moved package into own subfolder

---
+++ Calculator_iOS/Calculator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_iOSApp.swift
+//  Calculator_iOS
+//
+//  Created by Philip Niedertscheider on 12.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator_iOS/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Calculator_iOS
+//
+//  Created by Philip Niedertscheider on 12.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State var number1 = ""
+    @State var op = "+"
+    @State var number2 = ""
+
+    var body: some View {
+        VStack {
+            TextField("Number 1", text: $number1)
+                .keyboardType(.numberPad)
+                .padding(10)
+                .cornerRadius(5)
+            Picker("Operator", selection: $op) {
+                ForEach(["+", "-", "*", "/"], id: \.self) { op in
+                    Text(op)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            TextField("Number 2", text: $number2)
+                .keyboardType(.numberPad)
+                .padding(10)
+                .cornerRadius(5)
+            Divider()
+            Text("Result: " + result)
+                .padding(10)
+        }
+        .padding(20)
+    }
+
+    var result: String {
+        return "?"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
